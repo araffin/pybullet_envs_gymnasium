c44651a1ff7fe5f88f2b4bc42ec9e9874798b080
Antonin Raffin <antonin.raffin@ensta.org>
2023-09-01 12:15:27 +0200

    Revert version bump

--- pybullet_envs_gymnasium/__init__.py
+++ pybullet_envs_gymnasium/__init__.py
@@ -3,42 +3,42 @@ from gymnasium.envs.registration import register
 # ------------bullet-------------
 
 register(
-    id="InvertedPendulumBulletEnv-v1",
+    id="InvertedPendulumBulletEnv-v0",
     entry_point="pybullet_envs_gymnasium.gym_pendulum_envs:InvertedPendulumBulletEnv",
     max_episode_steps=1000,
     reward_threshold=950.0,
 )
 
 register(
-    id="InvertedDoublePendulumBulletEnv-v1",
+    id="InvertedDoublePendulumBulletEnv-v0",
     entry_point="pybullet_envs_gymnasium.gym_pendulum_envs:InvertedDoublePendulumBulletEnv",
     max_episode_steps=1000,
     reward_threshold=9100.0,
 )
 
 register(
-    id="InvertedPendulumSwingupBulletEnv-v1",
+    id="InvertedPendulumSwingupBulletEnv-v0",
     entry_point="pybullet_envs_gymnasium.gym_pendulum_envs:InvertedPendulumSwingupBulletEnv",
     max_episode_steps=1000,
     reward_threshold=800.0,
 )
 
 register(
-    id="ReacherBulletEnv-v1",
+    id="ReacherBulletEnv-v0",
     entry_point="pybullet_envs_gymnasium.gym_manipulator_envs:ReacherBulletEnv",
     max_episode_steps=150,
     reward_threshold=18.0,
 )
 
 register(
-    id="PusherBulletEnv-v1",
+    id="PusherBulletEnv-v0",
     entry_point="pybullet_envs_gymnasium.gym_manipulator_envs:PusherBulletEnv",
     max_episode_steps=150,
     reward_threshold=18.0,
 )
 
 register(
-    id="ThrowerBulletEnv-v1",
+    id="ThrowerBulletEnv-v0",
     entry_point="pybullet_envs_gymnasium.gym_manipulator_envs:ThrowerBulletEnv",
     max_episode_steps=100,
     reward_threshold=18.0,
@@ -46,47 +46,47 @@ register(
 
 
 register(
-    id="Walker2DBulletEnv-v1",
+    id="Walker2DBulletEnv-v0",
     entry_point="pybullet_envs_gymnasium.gym_locomotion_envs:Walker2DBulletEnv",
     max_episode_steps=1000,
     reward_threshold=2500.0,
 )
 register(
-    id="HalfCheetahBulletEnv-v1",
+    id="HalfCheetahBulletEnv-v0",
     entry_point="pybullet_envs_gymnasium.gym_locomotion_envs:HalfCheetahBulletEnv",
     max_episode_steps=1000,
     reward_threshold=3000.0,
 )
 
 register(
-    id="AntBulletEnv-v1",
+    id="AntBulletEnv-v0",
     entry_point="pybullet_envs_gymnasium.gym_locomotion_envs:AntBulletEnv",
     max_episode_steps=1000,
     reward_threshold=2500.0,
 )
 
 register(
-    id="HopperBulletEnv-v1",
+    id="HopperBulletEnv-v0",
     entry_point="pybullet_envs_gymnasium.gym_locomotion_envs:HopperBulletEnv",
     max_episode_steps=1000,
     reward_threshold=2500.0,
 )
 
 register(
-    id="HumanoidBulletEnv-v1",
+    id="HumanoidBulletEnv-v0",
     entry_point="pybullet_envs_gymnasium.gym_locomotion_envs:HumanoidBulletEnv",
     max_episode_steps=1000,
 )
 
 register(
-    id="HumanoidFlagrunBulletEnv-v1",
+    id="HumanoidFlagrunBulletEnv-v0",
     entry_point="pybullet_envs_gymnasium.gym_locomotion_envs:HumanoidFlagrunBulletEnv",
     max_episode_steps=1000,
     reward_threshold=2000.0,
 )
 
 register(
-    id="HumanoidFlagrunHarderBulletEnv-v1",
+    id="HumanoidFlagrunHarderBulletEnv-v0",
     entry_point="pybullet_envs_gymnasium.gym_locomotion_envs:HumanoidFlagrunHarderBulletEnv",
     max_episode_steps=1000,
 )
